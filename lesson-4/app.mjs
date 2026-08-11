23465a84232028add046458b02976b218dbaff2b
Kolya Kolchenko <kk.frl.webdev@gmail.com>
2026-08-11 17:28:38 +0300

    Implement book manager with CRUD functionality and data handling

--- lesson-4/app.mjs
+++ lesson-4/app.mjs
@@ -1,43 +1,50 @@
-import express from 'express'
-import path from 'path'
-import cookieParser from 'cookie-parser'
-import logger from 'morgan'
-import { fileURLToPath } from 'url'
+// Задача. Створіть “Менеджер книг” (books_manager):
+// CRUD-додаток для роботи з бібліотекою книг.
+//  Кожна книга має назву, автора, рік видання.
+//  Дані зберігайте у файлі books.json.
+//  Додайте пошук за автором та фільтрацію за роком.
+//  Використовуйте шаблонізатор EJS.
 
-import indexRouter from './routes/index.mjs'
-import usersRouter from './routes/users.mjs'
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import { fileURLToPath } from "url";
 
-const app = express()
-const __filename = fileURLToPath(import.meta.url) // get the resolved path to the file
-const __dirname = path.dirname(__filename) // get the name of the directory
+import indexRouter from "./routes/index.mjs";
+import usersRouter from "./routes/users.mjs";
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+const app = express();
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
 
 // error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
-export default app
+  res.status(err.status || 500);
+  res.render("error");
+});
+export default app;
